e06f1718e880ddcca617fee488ef59868271515d
maugarciav <mau.villanueva2002@gmail.com>
2023-11-23 11:34:15 -0600

    Initial Commit

---
+++ ExamenCOVID/ExamenCOVIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenCOVIDApp.swift
+//  ExamenCOVID
+//
+//  Created by Mauricio Garcia Villanueva on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamenCOVIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

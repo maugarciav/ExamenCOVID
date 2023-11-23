404fddccaa63fa82ff343e2599518fdc8610743a
maugarciav <mau.villanueva2002@gmail.com>
2023-11-23 13:51:09 -0600

    add: documents and conection to api

---
+++ ExamenCOVID/Data/CovidRepository.swift
@@ -0,0 +1,39 @@
+//
+//  CovidRepository.swift
+//  ExamenCOVID
+//
+//  Created by Mauricio Garcia Villanueva on 23/11/23.
+//
+
+
+
+
+import Foundation
+
+
+
+struct Api {
+    static let base = "https://api.api-ninjas.com/v1/covid19?date=2022-01-01"
+}
+
+
+protocol CovidAPIProtocol {
+    func getCovidList() async -> [CovidData]?
+}
+
+
+class CovidRepository: CovidAPIProtocol {
+    
+    let nservice: NetworkApiService
+    
+    static let shared = CovidRepository()
+
+    init(nservice: NetworkApiService = NetworkApiService.shared) {
+        self.nservice = nservice
+    }
+
+
+    func getCovidList() async -> [CovidData]? {
+        return await nservice.getCovidList(url: URL(string:"\(Api.base)")!)
+    }
+}
---
+++ ExamenCOVID/Data/Modelos/Network/NetworkApiService.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkApiService.swift
+//  ExamenCOVID
+//
+//  Created by Mauricio Garcia Villanueva on 23/11/23.
+//
+
+import Foundation
+import Alamofire
+
+
+
+class NetworkApiService {
+    static let shared = NetworkApiService()
+
+
+    func getCovidList(url: URL) async -> [CovidData]? {
+
+        let headers: HTTPHeaders = [
+            "X-Api-Key" : "Z6awhwtT3z0TQEpbcp3TYA==rxFp6snKUhuzfHAQ",
+        ]
+        
+    
+        let taskRequest = AF.request(url, method: .get, headers: headers).validate()
+                let response = await taskRequest.serializingData().response
+                
+                switch response.result{
+                case .success(let data):
+                    do {
+                        let countries = try JSONDecoder().decode([CovidData].self, from: data)
+                        return countries
+                    } catch {
+                        return nil
+                    }
+                case let .failure(error):
+                    debugPrint(error.localizedDescription)
+                    return nil
+                }
+    }
+}
+
